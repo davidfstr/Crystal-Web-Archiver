c2687cac40191f0f237543952aea81beead98111
David Foster <davidfstr@gmail.com>
2011-10-09 12:29:27 -0700

    RootResource: Support creation. Provide string representations for Resource and RootResource.

--- crystal/__init__.py
+++ crystal/__init__.py
@@ -24,6 +24,7 @@ class Project(object):
         self.path = path
         
         self._resources = OrderedDict()         # <url, Resource>
+        self._root_resources = OrderedDict()    # <Resource, RootResource>
         
         self._loading = True
         try:
@@ -32,16 +33,20 @@ class Project(object):
                 self._db = sqlite3.connect(os.path.join(path, self._DB_FILENAME))
                 
                 c = self._db.cursor()
-                c.execute('select url, id from resource')
-                for (url, id) in c:
+                for (url, id) in c.execute('select url, id from resource'):
                     Resource(self, url, _id=id)
+                for (name, resource_id, id) in c.execute('select name, resource_id, id from root_resource'):
+                    resource = [r for r in self._resources if r._id == resource_id][0] # PERF
+                    RootResource(self, name, resource, _id=id)
             else:
                 # Create new project
                 os.mkdir(path)
                 os.mkdir(os.path.join(path, self._BLOBS_DIRNAME))
                 self._db = sqlite3.connect(os.path.join(path, self._DB_FILENAME))
                 
+                c = self._db.cursor()
                 c.execute('create table resource (id integer primary key, url text unique)')
+                c.execute('create table root_resource (id integer primary key, name text, resource_id integer unique, foreign key (resource_id) references resource(id))')
         finally:
             self._loading = False
 
@@ -49,6 +54,7 @@ class Resource(object):
     def __new__(cls, project, url, _id=None):
         """
         Arguments:
+        project -- associated `Project`.
         url -- absolute URL to this resource (ex: http), or a URI (ex: mailto).
         """
         
@@ -60,7 +66,7 @@ class Resource(object):
             self.url = url
             
             if project._loading:
-                self._id = _id;
+                self._id = _id
             else:
                 c = project._db.cursor()
                 c.execute('insert into resource (url) values (?)', (url,))
@@ -68,3 +74,47 @@ class Resource(object):
                 self._id = c.lastrowid
             project._resources[url] = self
             return self
+    
+    def __repr__(self):
+        return "Resource('%s')" % (self.url,)
+
+class RootResource(object):
+    def __new__(cls, project, name, resource, _id=None):
+        """
+        Arguments:
+        project -- associated `Project`.
+        name -- display name.
+        resource -- resource.
+        
+        Raises:
+        RootResource.AlreadyExists -- if there is already a `RootResource` associated
+                                      with the specified resource.
+        """
+        
+        if resource in project._root_resources:
+            raise RootResource.AlreadyExists
+        else:
+            self = object.__new__(cls)
+            self.project = project
+            self.name = name
+            self.resource = resource
+            
+            if project._loading:
+                self._id = _id
+            else:
+                c = project._db.cursor()
+                c.execute('insert into root_resource (name, resource_id) values (?, ?)', (name, resource._id))
+                project._db.commit()
+                self._id = c.lastrowid
+            project._root_resources[resource] = self
+            return self
+    
+    def __repr__(self):
+        return "RootResource('%s', '%s')" % (self.name, self.resource.url)
+    
+    class AlreadyExists(Exception):
+        """
+        Raised when an attempt is made to create a new `RootResource` for a `Resource`
+        that is already associated with an existing `RootResource`.
+        """
+        pass
