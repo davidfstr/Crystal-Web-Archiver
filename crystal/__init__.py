cbb1ad13843fc330521d532c96b43944251fd50a
David Foster <davidfstr@gmail.com>
2011-10-09 01:08:48 -0700

    Resource: Support creation.

--- crystal/__init__.py
+++ crystal/__init__.py
@@ -1,3 +1,4 @@
+from collections import OrderedDict
 import os
 import sqlite3
 
@@ -17,21 +18,54 @@ class Project(object):
         Loads a project from the specified filepath, or creates a new one if none is found.
         
         Arguments:
-        path -- Path to a directory (ideally with the `FILE_EXTENSION` extension)
+        path -- path to a directory (ideally with the `FILE_EXTENSION` extension)
                 from which the project is to be loaded.
-        primary_domain -- Absolute URL string that the majority of URLs will be relative to.
+        primary_domain -- absolute URL string that the majority of URLs will be relative to.
                           URLs displayed in the UI will be converted to be relative to this
                           URL whenever possible.
         """
-        
         self.path = path
         self.primary_domain = primary_domain
         
-        if os.path.exists(path):
-            # Load from existing project
-            self._db = sqlite3.connect(os.path.join(path, self._DB_FILENAME))
+        self._resources = OrderedDict()
+        
+        self._loading = True
+        try:
+            if os.path.exists(path):
+                # Load from existing project
+                self._db = sqlite3.connect(os.path.join(path, self._DB_FILENAME))
+                
+                c = self._db.cursor()
+                c.execute('select url from resource')
+                for url in c:
+                    Resource(self, url)
+            else:
+                # Create new project
+                os.mkdir(path)
+                os.mkdir(os.path.join(path, self._BLOBS_DIRNAME))
+                self._db = sqlite3.connect(os.path.join(path, self._DB_FILENAME))
+                
+                c.execute('create table resource (id integer primary key, url text)')
+        finally:
+            self._loading = False
+
+class Resource(object):
+    def __new__(cls, project, url):
+        """
+        Arguments:
+        url -- absolute URL to this resource (ex: http), or a URI (ex: mailto).
+        """
+        
+        if url in project._resources:
+            return project._resources[url]
         else:
-            # Create new project
-            os.mkdir(path)
-            os.mkdir(os.path.join(path, self._BLOBS_DIRNAME))
-            self._db = sqlite3.connect(os.path.join(path, self._DB_FILENAME))
+            self = object.__new__(cls)
+            self.project = project
+            self.url = url
+            
+            if not project._loading:
+                c = project._db.cursor()
+                c.execute('insert into resource (url) values (?)', (url,))
+                project._db.commit()
+            project._resources[url] = self
+            return self
