eb1a55b737b3500f1f9654dd95937f3556168035
David Foster <davidfstr@gmail.com>
2011-10-09 17:13:34 -0700

    LinkParser: Support parsing Links from HTML.

--- crystal/__init__.py
+++ crystal/__init__.py
@@ -93,7 +93,7 @@ class Resource(object):
         return _ResourceBodyDownloadTask(self)
     
     def __repr__(self):
-        return "Resource('%s')" % (self.url,)
+        return "Resource(%s)" % (repr(self.url),)
 
 class RootResource(object):
     """
@@ -136,7 +136,7 @@ class RootResource(object):
             return self
     
     def __repr__(self):
-        return "RootResource('%s', '%s')" % (self.name, self.resource.url)
+        return "RootResource(%s,%s)" % (repr(self.name), repr(self.resource.url))
     
     class AlreadyExists(Exception):
         """
@@ -198,3 +198,89 @@ class ResourceRevision(object):
         self.download_error = download_error
         self._request = request             # TODO: finalize internal representation
         self._response_body = response_body # TODO: finalize internal representation
+
+# --------------------------------------------------------------------------------------------------
+
+from BeautifulSoup import BeautifulSoup
+import re
+
+class LinkParser(object):
+    _any_re = re.compile(r'.*')
+    
+    @staticmethod
+    def parse(html_bytes):
+        """
+        Parses and returns a list of `Link`s in the specified HTML bytestring.
+        """
+        # TODO: Pass in the hinted Content-Encoding HTTP header, if available,
+        #       to assist in determining the correct text encoding
+        try:
+            html = BeautifulSoup(html_bytes)
+        except Exception as e:
+            # TODO: Return the underlying exception as a warning
+            return []
+        
+        tags_with_src = html.findAll(LinkParser._any_re, src=LinkParser._any_re)
+        tags_with_href = html.findAll(LinkParser._any_re, href=LinkParser._any_re)
+        
+        links = []
+        for tag in tags_with_src:
+            # TODO: Need to resolve URLs to be absolute
+            url = tag['src']
+            if tag.name == 'img':
+                title = LinkParser._get_image_tag_title(tag)
+                links.append(Link(url, title, 'Image', True))
+            elif tag.name == 'frame':
+                title = tag['name'] if 'name' in tag.attrMap else None
+                links.append(Link(url, title, 'Frame', True))
+            elif tag.name == 'input' and 'type' in tag.attrMap and tag['type'] == 'image':
+                title = LinkParser._get_image_tag_title(tag)
+                links.append(Link(url, title, 'Form Image', True))
+            else:
+                title = None
+                links.append(Link(url, title, 'Unknown Embedded (%s)' % (tag.name,), True))
+        for tag in tags_with_href:
+            # TODO: Need to resolve URLs to be absolute
+            url = tag['href']
+            if tag.name == 'a':
+                title = tag.string
+                links.append(Link(url, title, 'Link', False))
+            elif tag.name == 'link' and (
+                    ('rel' in tag.attrMap and tag['rel'] == 'stylesheet') or (
+                     'type' in tag.attrMap and tag['type'] == 'text/css') or (
+                     url.endswith('.css'))):
+                title = None
+                links.append(Link(url, title, 'Stylesheet', True))
+            else:
+                title = None
+                links.append(Link(url, title, 'Unknown (%s)' % (tag.name,), False))
+        
+        return links
+    
+    @staticmethod
+    def _get_image_tag_title(tag):
+        if 'alt' in tag.attrMap:
+            return tag['alt']
+        elif 'title' in tag.attrMap:
+            return tag['title']
+        else:
+            return None
+
+class Link(object):
+    def __init__(self, url, title, type_title, embedded):
+        """
+        Arguments:
+        url - URL or URI referenced by this link.
+        title - displayed title for this link, or None.
+        type_title - displayed title for this link's type.
+        embedded - whether this link refers to an embedded resource.
+        """
+        if url is None or type_title is None or embedded not in (True, False):
+            raise ValueError
+        self.url = url
+        self.title = title
+        self.type_title = type_title
+        self.embedded = embedded
+    
+    def __repr__(self):
+        return 'Link(%s,%s,%s,%s)' % (repr(self.url), repr(self.title), repr(self.type_title), repr(self.embedded))
