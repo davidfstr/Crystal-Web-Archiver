a3925b149f83eef8f88093cc0106e061d7a32f47
David Foster <davidfstr@gmail.com>
2011-10-16 12:54:07 -0700

    ui: Simplify 'ui_call_later' implementation.

--- crystal/ui.py
+++ crystal/ui.py
@@ -8,23 +8,9 @@ import wx
 # Single global app object
 APP = wx.PySimpleApp()
 
-_EVT_CALLNOW_ID = wx.NewId()
-
-class _CallNowEvent(wx.PyEvent):
-    def __init__(self, callable):
-        wx.PyEvent.__init__(self)
-        self.SetEventType(_EVT_CALLNOW_ID)
-        self.callable = callable
-
 def ui_call_later(callable):
     """
     Calls the argument on the UI thread.
     This should be used for any operation that needs to access UI elements.
     """
-    wx.PostEvent(APP, _CallNowEvent(callable))
-
-# ------------------------------------------------------------------------------
-
-def _OnCallNow(event):
-    event.callable()
-APP.Connect(-1, -1, _EVT_CALLNOW_ID, _OnCallNow)
+    wx.CallAfter(callable)
