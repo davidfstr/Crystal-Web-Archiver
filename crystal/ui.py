ef79cbacea0a9e04d6f297acb27b65640c55469b
David Foster <davidfstr@gmail.com>
2011-10-15 19:39:23 -0700

    ui: Refactor internals.

--- crystal/ui.py
+++ crystal/ui.py
@@ -2,14 +2,18 @@
 Contains utility functions for manipulating the UI.
 """
 
+# Initialize wx, if not already done
 import wx
 
-_EVT_CALLUI_ID = wx.NewId()
+# Single global app object
+APP = wx.PySimpleApp()
 
-class _CallUIEvent(wx.PyEvent):
+_EVT_CALLNOW_ID = wx.NewId()
+
+class _CallNowEvent(wx.PyEvent):
     def __init__(self, callable):
         wx.PyEvent.__init__(self)
-        self.SetEventType(_EVT_CALLUI_ID)
+        self.SetEventType(_EVT_CALLNOW_ID)
         self.callable = callable
 
 def ui_call_later(callable):
@@ -17,13 +21,10 @@ def ui_call_later(callable):
     Calls the argument on the UI thread.
     This should be used for any operation that needs to access UI elements.
     """
-    wx.PostEvent(APP, _CallUIEvent(callable))
+    wx.PostEvent(APP, _CallNowEvent(callable))
 
 # ------------------------------------------------------------------------------
 
-# Single global app object
-APP = wx.PySimpleApp()
-
-def _OnCallUI(event):
+def _OnCallNow(event):
     event.callable()
-APP.Connect(-1, -1, _EVT_CALLUI_ID, _OnCallUI)
+APP.Connect(-1, -1, _EVT_CALLNOW_ID, _OnCallNow)
