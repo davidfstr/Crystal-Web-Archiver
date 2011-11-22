ffcb646c6dc983ffe2ef0f8455dafa94996158cd
David Foster <davidfstr@gmail.com>
2011-11-22 10:22:58 -0800

    xthreading: Extend *_call_* methods with optional argument lists.

--- crystal/xthreading.py
+++ crystal/xthreading.py
@@ -15,17 +15,17 @@ import wx
 # when it isn't desirable to create a wx.App object.
 _ASSUME_CURRENT_THREAD_IS_FOREGROUND = False
 
-def fg_call_later(callable):
+def fg_call_later(callable, *args):
     """
     Schedules the argument to be called on the foreground thread.
     This should be called by background threads that need to access the UI or model.
     """
     if _ASSUME_CURRENT_THREAD_IS_FOREGROUND or wx.Thread_IsMain():
-        callable()
+        callable(*args)
     else:
-        wx.CallAfter(callable)
+        wx.CallAfter(callable, *args)
 
-def fg_call_and_wait(callable):
+def fg_call_and_wait(callable, *args):
     """
     Calls the argument on the foreground thread and waits for it to complete.
     This should be called by background threads that need to access the UI or model.
@@ -34,7 +34,7 @@ def fg_call_and_wait(callable):
     If the callable raises an exception, it will be reraised by this method.
     """
     if _ASSUME_CURRENT_THREAD_IS_FOREGROUND or wx.Thread_IsMain():
-        callable()
+        callable(*args)
     else:
         condition = threading.Condition()
         callable_result = [None]
@@ -43,7 +43,7 @@ def fg_call_and_wait(callable):
         def fg_task():
             # Run task
             try:
-                callable_result[0] = callable()
+                callable_result[0] = callable(*args)
             except BaseException as e:
                 callable_exc_info[0] = sys.exc_info()
             
@@ -66,8 +66,8 @@ def fg_call_and_wait(callable):
         return callable_result[0]
         
 
-def bg_call_later(callable):
+def bg_call_later(callable, *args):
     """
     Calls the argument on a new background thread.
     """
-    threading.Thread(target=callable).start()
+    threading.Thread(target=callable, args=args).start()
