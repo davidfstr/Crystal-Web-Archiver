3838628365f110f2a3905bce532bafef66cef350
David Foster <davidfstr@gmail.com>
2011-12-04 16:58:36 -0800

    Add tree CLI that supports subtitles.

---
+++ crystal/ui/tree2cli.py
@@ -0,0 +1,64 @@
+"""
+Provides a tree UI with an API similar to the 'tree' module, but with two differences:
+* subtitles are supported (in addition to the regular title)
+* the UI is a CLI instead of a GUI.
+"""
+
+class TreeView(object):
+    def __init__(self):
+        self._root = NodeView()
+        self._root._attach(self)
+    
+    @property
+    def root(self):
+        return self._root
+    
+    def _refresh(self):
+        def _print_node(parent, level):
+            print '%s* %s' % ('  '*level, parent.title or '-')
+            print '%s  %s' % ('  '*level, parent.subtitle or '-')
+            for child in parent.children:
+                _print_node(child, level+1)
+        
+        _print_node(self.root, 0)
+        print
+
+class NodeView(object):
+    def __init__(self):
+        self._tree = None
+        self._title = ''
+        self._subtitle = ''
+        self._children = []
+    
+    def _get_title(self):
+        return self._title
+    def _set_title(self, value):
+        self._title = value
+        if self._tree:
+            self._tree._refresh()
+    title = property(_get_title, _set_title)
+    
+    def _get_subtitle(self):
+        return self._subtitle
+    def _set_subtitle(self, value):
+        self._subtitle = value
+        if self._tree:
+            self._tree._refresh()
+    subtitle = property(_get_subtitle, _set_subtitle)
+    
+    def _get_children(self):
+        return self._children
+    def _set_children(self, value):
+        self._children = value
+        if self._tree:
+            self._attach_children()
+            self._tree._refresh()
+    children = property(_get_children, _set_children)
+    
+    def _attach(self, tree):
+        self._tree = tree
+        self._attach_children()
+    
+    def _attach_children(self):
+        for child in self._children:
+            child._attach(self._tree)
