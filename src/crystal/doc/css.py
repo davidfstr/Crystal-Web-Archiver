5c5ec1e23796729ee132a26bedd244bba5fc7f4a
David Foster <david@dafoster.net>
2021-03-15 12:31:08 -0700

    Can identify url("*") references inside CSS.
    Helps download & serve: http://themanime.org/

--- src/crystal/doc/css.py
+++ src/crystal/doc/css.py
@@ -7,7 +7,7 @@ from __future__ import annotations
 from crystal.doc.generic import Document, Link
 import tinycss2
 from tinycss2 import ast
-from tinycss2.serializer import serialize_url
+from tinycss2.serializer import serialize_string_value, serialize_url
 
 
 def parse_css_and_links(
@@ -22,8 +22,17 @@ def parse_css_and_links(
     for rule in rules:
         if isinstance(rule, ast.QualifiedRule) or isinstance(rule, ast.AtRule):
             for token in rule.content:
+                # url(**)
                 if isinstance(token, ast.URLToken):
                     links.append(UrlTokenLink(token))
+                
+                # url("**")
+                elif isinstance(token, ast.FunctionBlock):
+                    if (token.lower_name == 'url' and 
+                            len(token.arguments) == 1 and 
+                            isinstance(token.arguments[0], ast.StringToken)):
+                        links.append(UrlFunctionLink(token.arguments[0]))
+    
     return (CssDocument(rules), links)
 
 
@@ -36,7 +45,7 @@ class CssDocument(Document):
 
 
 class UrlTokenLink(Link):
-    def __init__(self, token) -> None:
+    def __init__(self, token: ast.URLToken) -> None:
         self._token = token
         
         self.title = None
@@ -48,4 +57,20 @@ class UrlTokenLink(Link):
     def _set_relative_url(self, url: str) -> None:
         self._token.value = url
         self._token.representation = 'url(%s)' % serialize_url(url)
+    relative_url = property(_get_relative_url, _set_relative_url)
+
+
+class UrlFunctionLink(Link):
+    def __init__(self, token: ast.StringToken) -> None:
+        self._token = token
+        
+        self.title = None
+        self.type_title = 'CSS URL Reference'
+        self.embedded = True
+    
+    def _get_relative_url(self) -> str:
+        return self._token.value
+    def _set_relative_url(self, url: str) -> None:
+        self._token.value = url
+        self._token.representation = serialize_string_value(url)
     relative_url = property(_get_relative_url, _set_relative_url)
