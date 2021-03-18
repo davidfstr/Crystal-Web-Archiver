7f6f35c2c4f141665303935e23609e3de3505a3b
David Foster <david@dafoster.net>
2021-03-18 08:13:49 -0700

    make-mac: Use correct version number.

--- setup/make-mac.sh
+++ setup/make-mac.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
+VERSION=`python3 -c 'import crystal; print(crystal.__version__)'`
+
 rm -rf build dist dist-mac
 python setup.py py2app
 mkdir dist-mac
-hdiutil create -srcfolder dist -volname "Crystal Web Archiver" -format UDZO dist-mac/crystal-mac-1.0.dmg
+hdiutil create -srcfolder dist -volname "Crystal Web Archiver" -format UDZO dist-mac/crystal-mac-$VERSION.dmg
