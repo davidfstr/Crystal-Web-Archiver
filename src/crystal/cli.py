47567fac1a351dd92a9891b6d5554daabe27edbf
David Foster <david@dafoster.net>
2022-05-30 12:49:33 -0700

    Refactor extract cli.py

---
+++ src/crystal/cli.py
@@ -0,0 +1,35 @@
+# ----------------------------------------------------------------------------------------
+# Terminal Colors
+
+_USE_COLORS = True
+
+# ANSI color codes
+# Obtained from: http://www.bri1.com/files/06-2008/pretty.py
+_TERM_FG_BLUE =          '\033[0;34m'
+_TERM_FG_BOLD_BLUE =     '\033[1;34m'
+_TERM_FG_RED =           '\033[0;31m'
+_TERM_FG_BOLD_RED =      '\033[1;31m'
+_TERM_FG_GREEN =         '\033[0;32m'
+_TERM_FG_BOLD_GREEN =    '\033[1;32m'
+_TERM_FG_CYAN =          '\033[0;36m'
+_TERM_FG_BOLD_CYAN =     '\033[1;36m'
+_TERM_FG_YELLOW =        '\033[0;33m'
+_TERM_FG_BOLD_YELLOW =   '\033[1;33m'
+_TERM_RESET =            '\033[0m'
+
+def print_success(message: str) -> None:
+    print(_colorize(_TERM_FG_GREEN, message))
+
+def print_error(message: str) -> None:
+    print(_colorize(_TERM_FG_RED, message))
+
+def print_warning(message: str) -> None:
+    print(_colorize(_TERM_FG_YELLOW, message))
+
+def print_info(message: str) -> None:
+    print(_colorize(_TERM_FG_CYAN, message))
+
+def _colorize(color_code, str_value):
+    return (color_code + str_value + _TERM_RESET) if _USE_COLORS else str_value
+
+# ------------------------------------------------------------------------------
--- src/crystal/server.py
+++ src/crystal/server.py
@@ -3,6 +3,12 @@ Implements an HTTP server that serves resource revisions from a Project.
 Runs on its own daemon thread.
 """
 
+from crystal.cli import (
+    print_error,
+    print_info,
+    print_success,
+    print_warning,
+)
 from crystal.model import Resource
 from crystal.task import schedule_forever
 from datetime import datetime
@@ -603,39 +609,5 @@ class _RequestHandler(BaseHTTPRequestHandler):
     
     def log_message(self, format, *args):  # override
         print_info(format % args)
-        
-# ----------------------------------------------------------------------------------------
-# Terminal Colors
-
-_USE_COLORS = True
-
-# ANSI color codes
-# Obtained from: http://www.bri1.com/files/06-2008/pretty.py
-_TERM_FG_BLUE =          '\033[0;34m'
-_TERM_FG_BOLD_BLUE =     '\033[1;34m'
-_TERM_FG_RED =           '\033[0;31m'
-_TERM_FG_BOLD_RED =      '\033[1;31m'
-_TERM_FG_GREEN =         '\033[0;32m'
-_TERM_FG_BOLD_GREEN =    '\033[1;32m'
-_TERM_FG_CYAN =          '\033[0;36m'
-_TERM_FG_BOLD_CYAN =     '\033[1;36m'
-_TERM_FG_YELLOW =        '\033[0;33m'
-_TERM_FG_BOLD_YELLOW =   '\033[1;33m'
-_TERM_RESET =            '\033[0m'
-
-def colorize(color_code, str_value):
-    return (color_code + str_value + _TERM_RESET) if _USE_COLORS else str_value
-
-def print_success(message: str) -> None:
-    print(colorize(_TERM_FG_GREEN, message))
-
-def print_error(message: str) -> None:
-    print(colorize(_TERM_FG_RED, message))
-
-def print_warning(message: str) -> None:
-    print(colorize(_TERM_FG_YELLOW, message))
-
-def print_info(message: str) -> None:
-    print(colorize(_TERM_FG_CYAN, message))
 
 # ------------------------------------------------------------------------------
