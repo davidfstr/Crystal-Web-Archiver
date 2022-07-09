976ae4be094ddb83dcf770bb6a955b5f84f9dd15
David Foster <david@dafoster.net>
2022-07-09 07:35:14 -0700

    make-win: Exit upon failure of any individual step

--- setup/make-win.bat
+++ setup/make-win.bat
@@ -1,6 +1,17 @@
 set INNO_COMPILER="C:\Program Files (x86)\Inno Setup 5\Compil32.exe"
 
 rmdir /s /q build dist dist-win
+
 call poetry run python setup.py py2exe
+IF %ERRORLEVEL% NEQ 0 (
+    echo "py2exe failed."
+    exit %ERRORLEVEL%
+)
+
 copy media\vcruntime140.dll dist\vcruntime140.dll
+
 %INNO_COMPILER% /cc win-installer.iss
+IF %ERRORLEVEL% NEQ 0 (
+    echo "Inno Setup compiler failed."
+    exit %ERRORLEVEL%
+)
