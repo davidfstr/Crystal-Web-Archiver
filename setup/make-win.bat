2120b9c927965df5be734ba87b870a1022b24db8
David Foster <david@dafoster.net>
2022-07-10 07:42:27 -0700

    make-win: Show built files and update installer script dynamically.
    Also:
    * make-win: Describe steps taken while running

--- setup/make-win.bat
+++ setup/make-win.bat
@@ -1,17 +1,27 @@
-set INNO_COMPILER="C:\Program Files (x86)\Inno Setup 6\ISCC.exe"
+@set INNO_COMPILER="C:\Program Files (x86)\Inno Setup 6\ISCC.exe"
 
+@echo Clearing dist...
 rmdir /s /q build dist dist-win
 
+@echo Building executable...
 call poetry run python setup.py py2exe
-IF %ERRORLEVEL% NEQ 0 (
-    echo "py2exe failed."
-    exit %ERRORLEVEL%
+@IF %ERRORLEVEL% NEQ 0 (
+    @echo "py2exe failed."
+    @exit %ERRORLEVEL%
 )
 
+@echo Copying in C runtime DLL...
 copy media\vcruntime140.dll dist\vcruntime140.dll
 
+@echo Built files:
+dir dist
+
+@echo Regenerating list of built files in installer script...
+call poetry run python make_win_installer.py
+
+@echo Building installer...
 %INNO_COMPILER% win-installer.iss
-IF %ERRORLEVEL% NEQ 0 (
-    echo "Inno Setup compiler failed."
-    exit %ERRORLEVEL%
+@IF %ERRORLEVEL% NEQ 0 (
+    @echo "Inno Setup compiler failed."
+    @exit %ERRORLEVEL%
 )
