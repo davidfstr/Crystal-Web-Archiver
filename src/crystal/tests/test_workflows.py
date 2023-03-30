7de3fdb89e1e9826d9a389b79671ae2f16418ae1
David Foster <david@dafoster.net>
2023-03-29 21:21:12 -0400

    Tests: Refactor extract served_project to utility module

--- src/crystal/tests/test_workflows.py
+++ src/crystal/tests/test_workflows.py
@@ -1,10 +1,7 @@
 from __future__ import annotations
 
-from contextlib import contextmanager
-from copy import deepcopy
 from crystal.model import Project
-from crystal.server import get_request_url, ProjectServer
-import crystal.tests.test_data as test_data
+from crystal.server import get_request_url
 from crystal.tests.util.console import console_output_copied
 from crystal.tests.util.controls import (
     click_button, set_checkbox_value, TreeItem
@@ -12,32 +9,25 @@ from crystal.tests.util.controls import (
 from crystal.tests.util.runner import bg_fetch_url
 from crystal.tests.util.server import (
     assert_does_open_webbrowser_to, fetch_archive_url,
-    is_url_not_in_archive,
+    is_url_not_in_archive, served_project,
 )
 from crystal.tests.util.tasks import wait_for_download_to_start_and_finish
 from crystal.tests.util.wait import (
     DEFAULT_WAIT_TIMEOUT,
     first_child_of_tree_item_is_not_loading_condition,
     tree_has_no_children_condition,
-    wait_for, window_condition,
+    wait_for,
 )
 from crystal.tests.util.windows import (
     AddGroupDialog, AddUrlDialog, EntityTree,
     MainWindow, OpenOrCreateDialog, PreferencesDialog,
 )
-from crystal.util import http_date
-from crystal.util.xdatetime import datetime_is_aware
 from crystal.util.xthreading import is_foreground_thread
 import datetime
-import json
-import os
 import re
 import tempfile
-from typing import Iterator, List, Optional, Union
+from typing import List, Union
 from unittest import skip
-from urllib.parse import urljoin
-import wx
-from zipfile import ZipFile
 
 
 # ------------------------------------------------------------------------------
@@ -1026,68 +1016,6 @@ async def test_can_download_and_serve_a_site_requiring_cookie_authentication() -
 
 # ------------------------------------------------------------------------------
 # Utility
-
-@contextmanager
-def served_project(
-        zipped_project_filename: str,
-        *, fetch_date_of_resources_set_to: Optional[datetime.datetime]=None,
-        ) -> Iterator[ProjectServer]:
-    if fetch_date_of_resources_set_to is not None:
-        if not datetime_is_aware(fetch_date_of_resources_set_to):
-            raise ValueError('Expected fetch_date_of_resources_set_to to be an aware datetime')
-    
-    with tempfile.TemporaryDirectory() as project_parent_dirpath:
-        # Extract project
-        with test_data.open_binary(zipped_project_filename) as zipped_project_file:
-            with ZipFile(zipped_project_file, 'r') as project_zipfile:
-                project_zipfile.extractall(project_parent_dirpath)
-        
-        # Open project
-        (project_filename,) = [
-            fn for fn in os.listdir(project_parent_dirpath)
-            if fn.endswith('.crystalproj')
-        ]
-        project_filepath = os.path.join(project_parent_dirpath, project_filename)
-        with Project(project_filepath, readonly=True) as project:
-            # Alter the fetch date of every ResourceRevision in the project
-            # to match "fetch_date_of_resources_set_to", if provided
-            if fetch_date_of_resources_set_to is not None:
-                for r in project.resources:
-                    for rr in r.revisions():
-                        if rr.metadata is None:
-                            print(
-                                f'Warning: Unable to alter fetch date of '
-                                f'resource revision lacking HTTP headers: {rr}')
-                            continue
-                        
-                        rr_new_date = http_date.format(fetch_date_of_resources_set_to)
-                        
-                        # New Metadata = Old Metadata with Date and Age headers replaced
-                        rr_new_metadata = deepcopy(rr.metadata)
-                        rr_new_metadata['headers'] = [
-                            (cur_name, cur_value)
-                            for (cur_name, cur_value) in
-                            rr_new_metadata['headers']
-                            if cur_name.lower() not in ['date', 'age']
-                        ] + [('Date', rr_new_date)]
-                        
-                        # Alter ResourceRevision's metadata in memory
-                        rr.metadata = rr_new_metadata
-                        
-                        # Alter ResourceRevision's metadata in database
-                        c = project._db.cursor()
-                        c.execute(
-                            'update resource_revision set metadata = ? where id = ?',
-                            (json.dumps(rr_new_metadata), rr._id),  # type: ignore[attr-defined]
-                            ignore_readonly=True)
-                        project._db.commit()
-            
-            # Start server
-            yield project.start_server(
-                port=2798,  # CRYT on telephone keypad
-                verbosity='indent',
-            )
-
 
 async def _undownload_url(
         url_or_urls: Union[str, List[str]],
