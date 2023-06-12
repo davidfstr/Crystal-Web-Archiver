b1475dcf99d27459727d6a26c4abf7d82440154f
David Foster <david@dafoster.net>
2023-06-12 10:51:03 -0400

    Build: Mac: Support building only the .app, skipping the .dmg

--- setup/make-mac.sh
+++ setup/make-mac.sh
@@ -5,4 +5,7 @@ VERSION=`python -c 'import sys; sys.path.append("../src"); import crystal; print
 rm -rf build dist dist-mac
 poetry run python setup.py py2app
 mkdir dist-mac
-hdiutil create -srcfolder dist -volname "Crystal Web Archiver" -format UDZO dist-mac/crystal-mac-$VERSION.dmg
+if [ "$1" != "--app-only" ]; then
+    echo 'Building disk image... (skip with --app-only option)'
+    hdiutil create -srcfolder dist -volname "Crystal Web Archiver" -format UDZO dist-mac/crystal-mac-$VERSION.dmg
+fi
