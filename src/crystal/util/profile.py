372fe9adac3477fa6d9d8a21dbed7042cddc23e8
David Foster <david@dafoster.net>
2023-07-06 07:22:12 -0400

    Slow Tasks: Refactor create_profiled_callable() to use warn_if_slow() internally

--- src/crystal/util/profile.py
+++ src/crystal/util/profile.py
@@ -32,36 +32,34 @@ def warn_if_slow(
             ), file=sys.stderr)
 
 
-def create_profiled_callable(title: str, max_duration: float, callable, *args):
+def create_profiled_callable(title: str, max_duration: float, callable: Callable, *args) -> Callable:
     """
     Decorates the specified callable such that it prints
     a warning to the console if its runtime is long.
     """
-    def profiled_callable():
-        start_time = time.time()
-        try:
-            callable(*args)
-        finally:
-            end_time = time.time()
-            delta_time = end_time - start_time
-            if delta_time > max_duration:
-                root_callable = callable
-                while hasattr(root_callable, 'callable'):
-                    root_callable = root_callable.callable
-                
-                try:
-                    file = inspect.getsourcefile(root_callable)
-                except Exception:
-                    file = '?'
-                try:
-                    start_line_number = inspect.getsourcelines(root_callable)[-1]
-                except Exception:
-                    start_line_number = '?'
-                print("*** %s took %.02fs to execute: %s @ [%s:%s]" % (
-                    title,
-                    delta_time,
-                    root_callable,
-                    file,
-                    start_line_number
-                ), file=sys.stderr)
+    def profiled_callable() -> None:
+        def message_func() -> str:
+            # TODO: Remove support for nested callable objects that are
+            #       tracked using a "callable" attribute. I believe this
+            #       is dead code.
+            root_callable = callable
+            while hasattr(root_callable, 'callable'):
+                root_callable = root_callable.callable  # type: ignore[attr-defined]
+            
+            try:
+                file = inspect.getsourcefile(root_callable)
+            except Exception:
+                file = '?'
+            try:
+                start_line_number = str(inspect.getsourcelines(root_callable)[-1])
+            except Exception:
+                start_line_number = '?'
+            return '%s @ [%s:%s]' % (
+                root_callable,
+                file,
+                start_line_number
+            )
+            
+        with warn_if_slow(title, max_duration, message_func):
+            return callable(*args)
     return profiled_callable
