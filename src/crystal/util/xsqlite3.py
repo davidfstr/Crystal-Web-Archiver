0eb50df1d1c87180b9979e5f8bf04a141cc4eb70
David Foster <david@dafoster.net>
2023-08-19 14:47:37 -0400

    Disable certain features when SQLite does not have JSON support

--- src/crystal/util/xsqlite3.py
+++ src/crystal/util/xsqlite3.py
@@ -6,3 +6,18 @@ def is_database_closed_error(e: object) -> bool:
         isinstance(e, sqlite3.ProgrammingError) and 
         str(e) == 'Cannot operate on a closed database.'
     )
+
+
+# HACK: Some Windows runners in GitHub Actions don't have a version of SQLite
+#       compiled with JSON for some reason. So some behavior must be altered
+#       in that case.
+def sqlite_has_json_support() -> bool:
+    with sqlite3.connect(':memory:') as db:
+        c = db.cursor()
+        try:
+            return list(c.execute('select json(1)')) == [('1',)]
+        except sqlite3.OperationalError as e:
+            if str(e) == 'no such function: json':
+                return False
+            else:
+                raise
