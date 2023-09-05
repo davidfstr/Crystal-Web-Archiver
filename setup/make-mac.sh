a59cdcf29a404b22ee08a611bb0599db3d589957
David Foster <david@dafoster.net>
2023-09-04 22:00:43 -0400

    make-mac: Output the module dependency graph during the build process

--- setup/make-mac.sh
+++ setup/make-mac.sh
@@ -3,7 +3,9 @@
 VERSION=`python -c 'import sys; sys.path.append("../src"); import crystal; print(crystal.__version__)'`
 
 rm -rf build dist dist-mac
-poetry run python setup.py py2app
+# --graph: Generates a GraphViz .dot file in the build directory that shows
+#          the calculated module dependency graph
+poetry run python setup.py py2app --graph
 mkdir dist-mac
 if [ "$1" != "--app-only" ]; then
     echo 'Building disk image... (skip with --app-only option)'
