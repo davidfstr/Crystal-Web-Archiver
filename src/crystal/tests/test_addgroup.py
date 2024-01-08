19290b6e7d5a6a2a1745a5e4136c8b688b98ac8c
David Foster <david@dafoster.net>
2024-01-08 09:58:26 -0500

    Test: Fix waits in: test_given_node_is_selected_in_entity_tree_when_press_new_group_button_then_dialog_appears_with_reasonable_suggested_source

--- src/crystal/tests/test_addgroup.py
+++ src/crystal/tests/test_addgroup.py
@@ -331,15 +331,19 @@ async def test_given_node_is_selected_in_entity_tree_when_press_new_group_button
                     home_rrn__feed_glrn__rss_feed_lrn, home_url, url_prefix)
                 
                 # Children of LinkedResourceNode (a kind of _ResourceNode) that is inside RootResourceNode
-                await _expand_node(home_rrn__archive_lrn, mw, will_download=True)
+                # HACK: Relies on Crystal's "Not in Archive" page to provide a
+                #       link to the original URL (which is offsite)
+                await _expand_node(home_rrn__archive_lrn, mw)  # not in archive
                 home_rrn__archive_lrn__offsite_cn = _find_child_by_title(
                     home_rrn__archive_lrn, 'Offsite')
                 
                 # Children of LinkedResourceNode (a kind of _ResourceNode) that is inside ClusterNode
-                await _expand_node(home_rrn__offsite_cn__twitter_lrn, mw, will_download=True)
+                # HACK: Relies on Crystal's "Not in Archive" page to provide a
+                #       link to the original URL (which is offsite)
+                await _expand_node(home_rrn__offsite_cn__twitter_lrn, mw)  # not in archive
                 home_rrn__offsite_cn__twitter_lrn__offsite_cn = _find_child_by_title(
                     home_rrn__offsite_cn__twitter_lrn, 'Offsite')
-                await _expand_node(home_rrn__embedded_cn__stylesheet_lrn)
+                await _expand_node(home_rrn__embedded_cn__stylesheet_lrn)  # already downloaded
                 home_rrn__embedded_cn__stylesheet_lrn__embedded_cn = _find_child_by_title(
                     home_rrn__embedded_cn__stylesheet_lrn, 'Embedded')
                 
