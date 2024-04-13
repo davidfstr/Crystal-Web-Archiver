034daec90c790e42e4eb8c4d1d839f050505346d
David Foster <david@dafoster.net>
2024-04-13 16:28:39 -0400

    WordPress: nggallery: Fix %2F in ?post_type=... URLs
    Helps parse & serve:
    * https://shoujo-manga.land/

--- src/crystal/plugins/wordpress.py
+++ src/crystal/plugins/wordpress.py
@@ -3,6 +3,8 @@ import re
 
 _URL_WITH_REPLYTOCOM = re.compile(r'^(https?://[^?]+)\?replytocom=[0-9]+/?$')
 
+_URL_WITH_POSTTYPE_SLASH = re.compile(r'^(https?://[^?]+\?post_type=[a-zA-Z_]*)((?:/|%2F)*)$')
+
 def normalize_url(old_url: str, **kwargs) -> str:
     # Normalize "reply to comment" links to strip out parameter,
     # which won't be used anyway if page is loaded with JavaScript
@@ -15,5 +17,11 @@ def normalize_url(old_url: str, **kwargs) -> str:
     if m is not None:
         # Chop off replytocom=... part
         return m.group(1)
+    
+    # NextGEN Gallery (nggallery):
+    # Prevent percent-encode of slash within ?post_type=... segment
+    m = _URL_WITH_POSTTYPE_SLASH.fullmatch(old_url)
+    if m is not None:
+        return m.group(1) + m.group(2).replace('%2F', '/')
     
     return old_url
