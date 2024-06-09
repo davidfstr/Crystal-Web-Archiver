6076ea52606870c5faea2db74517564e944f22d7
David Foster <david@dafoster.net>
2024-06-08 20:06:28 -0400

    CRYSTAL_IGNORE_USE_AFTER_FREE: Support detecting use-after-free errors in C/C++

--- src/crystal/util/wx_error.py
+++ src/crystal/util/wx_error.py
@@ -1,5 +1,18 @@
 from contextlib import contextmanager
+import os
 from typing import Callable, Iterator, NoReturn, Optional
+
+
+# Whether RuntimeError('wrapped C/C++ object of type ... has been deleted')
+# (or its sibling WindowDeletedError) is silently ignored.
+# 
+# It is unsafe to ignore these errors in general because they are raised
+# on only a best effort basis when a use-after-free error in C/C++ would
+# normally have happened and either crashed the process immediately
+# or corrupted memory, crashing the process later.
+_IGNORE_USE_AFTER_FREE = (
+    os.environ.get('CRYSTAL_IGNORE_USE_AFTER_FREE', 'True') == 'True'
+)
 
 
 def is_wrapped_object_deleted_error(e: Exception) -> bool:
@@ -13,30 +26,38 @@ def is_wrapped_object_deleted_error(e: Exception) -> bool:
 
 @contextmanager
 def wrapped_object_deleted_error_ignored() -> Iterator[None]:
-    try:
+    if _IGNORE_USE_AFTER_FREE:
+        try:
+            yield
+        except Exception as e:
+            if is_wrapped_object_deleted_error(e):
+                pass
+            else:
+                raise
+    else:
+        # May raise errors matching is_wrapped_object_deleted_error()
         yield
-    except Exception as e:
-        if is_wrapped_object_deleted_error(e):
-            pass
-        else:
-            raise
 
 
 @contextmanager
 def wrapped_object_deleted_error_raising(
         raiser: Optional[Callable[[], NoReturn]]=None
         ) -> Iterator[None]:
-    if raiser is None:
-        def default_raiser() -> NoReturn:
-            raise WindowDeletedError()
-        raiser = default_raiser
-    try:
+    if _IGNORE_USE_AFTER_FREE:
+        if raiser is None:
+            def default_raiser() -> NoReturn:
+                raise WindowDeletedError()
+            raiser = default_raiser
+        try:
+            yield
+        except Exception as e:
+            if is_wrapped_object_deleted_error(e):
+                raiser()
+            else:
+                raise
+    else:
+        # May raise errors matching is_wrapped_object_deleted_error()
         yield
-    except Exception as e:
-        if is_wrapped_object_deleted_error(e):
-            raiser()
-        else:
-            raise
             
 
 class WindowDeletedError(Exception):
