4823e81a45031fbb6c87b0dc979583f307a84c46
David Foster <david@dafoster.net>
2024-06-22 20:53:58 -0400

    xcgi: Revert to using real cgi module when Python <3.11

--- src/crystal/util/xcgi.py
+++ src/crystal/util/xcgi.py
@@ -1,12 +1,21 @@
-from email.message import EmailMessage
+import sys
 from typing import Dict, Tuple
 
 
-def parse_header(mime_header_value: str) -> Tuple[str, Dict]:
-    """
-    Parse a MIME header (such as Content-Type) into a main value and
-    a dictionary of parameters.
-    """
-    msg = EmailMessage()
-    msg['content-type'] = mime_header_value
-    return (msg.get_content_type(), msg['content-type'].params)
+# NOTE: py2app doesn't appear to bundle the email.message module correctly,
+#       which is used by the Python 3.11+ version of parse_header().
+#       
+#       Currently this doesn't matter because release builds of Crystal
+#       are compiled with Python 3.8, but may matter in the future.
+if sys.version_info >= (3, 11):
+    def parse_header(mime_header_value: str) -> Tuple[str, Dict]:
+        """
+        Parse a MIME header (such as Content-Type) into a main value and
+        a dictionary of parameters.
+        """
+        from email.message import EmailMessage
+        msg = EmailMessage()
+        msg['content-type'] = mime_header_value
+        return (msg.get_content_type(), msg['content-type'].params)
+else:
+    from cgi import parse_header
