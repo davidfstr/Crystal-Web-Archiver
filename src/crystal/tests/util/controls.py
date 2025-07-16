461794b3570fbe875b9053721ede2cb8c5d73bfd
David Foster <david@dafoster.net>
2025-07-15 21:45:53 -0400

    Tests: select_menuitem_now: Extend to support passing a wx.MenuItem alone

--- src/crystal/tests/util/controls.py
+++ src/crystal/tests/util/controls.py
@@ -69,7 +69,28 @@ def file_dialog_returning(filepath: str) -> Iterator[None]:
 # ------------------------------------------------------------------------------
 # Utility: Controls: wx.Menu
 
-def select_menuitem_now(menu: wx.Menu, menuitem_id: int) -> None:
+# TODO: Alter callers that use the {menu, menuitem_id} signature to use {menuitem} instead
+#       when possible because it requires less bookkeeping in the caller.
+def select_menuitem_now(
+        menu: wx.Menu | None = None,
+        menuitem_id: int | None = None,
+        *, menuitem: wx.MenuItem | None = None
+        ) -> None:
+    """
+    Selects a menu item immediately.
+    
+    Either both `menu` and `menuitem_id` must be specified, or `menuitem` must be.
+    """
+    if menuitem is None:
+        if menu is None or menuitem_id is None:
+            raise ValueError('Either both `menu` and `menuitem_id` must be specified, or `menuitem` must be specified')
+    else:
+        menu = menuitem.GetMenu()
+        menuitem_id = menuitem.GetId()
+        
+        assert menuitem.IsEnabled(), \
+            f'Menu item {menuitem.GetItemLabelText()!r} is not enabled'
+    
     # Process the related wx.EVT_MENU event immediately,
     # so that the event handler is called before the wx.Menu is disposed
     event = wx.MenuEvent(type=wx.EVT_MENU.typeId, id=menuitem_id, menu=None)
