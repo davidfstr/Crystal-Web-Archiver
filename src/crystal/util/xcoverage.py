76709559abba36c21e5c450f6c789d87be51ea48
David Foster <david@dafoster.net>
2025-10-20 08:47:37 -0400

    Code Coverage: Exclude @skip tests. Exclude test suites.
    Also:
    * Code Coverage: Move `coverage html` configuration to pyproject.toml,
      so that you can just run `coverage html` without specifying additional
      options

--- src/crystal/util/xcoverage.py
+++ src/crystal/util/xcoverage.py
@@ -1,12 +1,20 @@
-from collections.abc import Iterator
-from contextlib import contextmanager
-import os
+"""
+Code coverage gathering utilities.
+
+Top-level code in this module is marked with "# pragma: no cover" because it 
+must be loaded before coverage instrumentation is started, 
+so will never be observed as covered.
+"""
+
+from collections.abc import Iterator  # pragma: no cover
+from contextlib import contextmanager  # pragma: no cover
+import os  # pragma: no cover
 
 
-cov = None
+cov = None  # pragma: no cover
 
 
-@contextmanager
+@contextmanager  # pragma: no cover
 def collect_and_save() -> Iterator[None]:
     """
     Runs a context under code coverage, if the COVERAGE_RUN env var is set to a value.
@@ -25,7 +33,12 @@ def collect_and_save() -> Iterator[None]:
     cov = None  # export
 
 
-def switch_context(test_name: str = '') -> None:
+def switch_context(test_name: str = '') -> None:  # pragma: no cover
+    """
+    Changes the test name that subsequent executed lines will be attributed to.
+    
+    Omit the test name to attribute lines to the test runner itself.
+    """
     if cov is None:
         return
     cov.switch_context(test_name)
