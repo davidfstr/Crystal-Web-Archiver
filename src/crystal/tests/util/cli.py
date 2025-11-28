fb82548a6437114c698276e3f06909987999c5d5
David Foster <david@dafoster.net>
2025-11-27 21:07:15 -0500

    Tests: CLI: Bump default timeout for crystal_running_with_banner()

--- src/crystal/tests/util/cli.py
+++ src/crystal/tests/util/cli.py
@@ -212,7 +212,14 @@ def crystal_running_with_banner(
             
             if len(next_lines) == 0:
                 try:
-                    (lines_str, _) = read_until(crystal.stdout, ('\n', '>>> '), _drain_diagnostic=False)
+                    (lines_str, _) = read_until(
+                        crystal.stdout,
+                        ('\n', '>>> '),
+                        # 2.0s isn't enough on macOS locally when run in parallel
+                        # 4.0s isn't enough on macOS in CI when run in parallel
+                        timeout=6.0,
+                        _drain_diagnostic=False,
+                    )
                 except ReadUntilTimedOut as e:
                     raise e.plus(
                         f' Read before: {"".join(lines_found)!r} '
