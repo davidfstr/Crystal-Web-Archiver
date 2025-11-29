ff55f8b08d2ce52b044819ebb4c311c82d756f70
David Foster <david@dafoster.net>
2025-11-29 14:39:07 -0500

    serial_only: Refactor extract mark() to create arbitrary marking decorators

--- src/crystal/tests/util/mark.py
+++ src/crystal/tests/util/mark.py
@@ -8,11 +8,22 @@ _P = ParamSpec('_P')
 _R = TypeVar('_R')
 
 
-def slow(func: Callable[_P, _R]) -> Callable[_P, _R]:
-    """Decorator that skips tests marked 'slow' via CRYSTAL_EXCLUDE_TESTS_MARKED."""
-    @wraps(func)
-    def wrapped(*args: _P.args, **kwargs: _P.kwargs) -> _R:
-        if 'slow' in os.environ.get('CRYSTAL_EXCLUDE_TESTS_MARKED', '').split(','):
-            raise SkipTest("marked 'slow'")
-        return func(*args, **kwargs)
-    return wrapped
+def mark(label: str) -> Callable[[Callable[_P, _R]], Callable[_P, _R]]:
+    """
+    Creates a decorator that marks tests with the specified label.
+    
+    If the label is included in the environment variable
+    CRYSTAL_EXCLUDE_TESTS_MARKED then it will be skipped by `crystal test`.
+    """
+    def marked(func: Callable[_P, _R]) -> Callable[_P, _R]:
+        @wraps(func)
+        def wrapped(*args: _P.args, **kwargs: _P.kwargs) -> _R:
+            if label in os.environ.get('CRYSTAL_EXCLUDE_TESTS_MARKED', '').split(','):
+                raise SkipTest(f'marked {label!r}')
+            return func(*args, **kwargs)
+        return wrapped
+    return marked
+
+
+# Test decorators that mark the test with the same-named label
+slow = mark('slow')
