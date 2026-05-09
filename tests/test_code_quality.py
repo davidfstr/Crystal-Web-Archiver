132688258d230b3fbbda53c3f245a2e3db3cae30
David Foster <david@dafoster.net>
2026-05-09 11:47:42 -0400

    Tests: Simplify subprocess calls that expect text output

--- tests/test_code_quality.py
+++ tests/test_code_quality.py
@@ -6,36 +6,33 @@ import sys
 
 def test_type_checker_reports_no_errors() -> None:
     try:
-        output_bytes = subprocess.check_output(
+        subprocess.check_output(
             ['mypy'],
-            stderr=subprocess.STDOUT
+            stderr=subprocess.STDOUT,
+            encoding='utf-8',
         )
     except subprocess.CalledProcessError as e:
-        output_bytes = e.output
-        output = output_bytes.decode('utf-8')
-        fail('Typechecker failed with output:\n\n%s' % output.rstrip(), pytrace=False)
+        fail('Typechecker failed with output:\n\n%s' % e.output.rstrip(), pytrace=False)
 
 
 def test_linter_reports_no_diagnostics() -> None:
     try:
-        output_bytes = subprocess.check_output(
+        subprocess.check_output(
             'pylint src tests'.split(' '),
-            stderr=subprocess.STDOUT
+            stderr=subprocess.STDOUT,
+            encoding='utf-8',
         )
     except subprocess.CalledProcessError as e:
-        output_bytes = e.output
-        output = output_bytes.decode('utf-8')
-        fail('Linter failed with output:\n\n%s' % output.rstrip(), pytrace=False)
+        fail('Linter failed with output:\n\n%s' % e.output.rstrip(), pytrace=False)
 
 
 def test_that_zizmor_reports_no_github_action_workflow_vulnerabilities() -> None:
     zizmor = os.path.join(os.path.dirname(sys.executable), 'zizmor')
     try:
-        output_bytes = subprocess.check_output(
+        subprocess.check_output(
             [zizmor, '--persona', 'auditor', '.github/workflows/'],
-            stderr=subprocess.STDOUT
+            stderr=subprocess.STDOUT,
+            encoding='utf-8',
         )
     except subprocess.CalledProcessError as e:
-        output_bytes = e.output
-        output = output_bytes.decode('utf-8')
-        fail('zizmor found vulnerabilities:\n\n%s' % output.rstrip(), pytrace=False)
+        fail('zizmor found vulnerabilities:\n\n%s' % e.output.rstrip(), pytrace=False)
